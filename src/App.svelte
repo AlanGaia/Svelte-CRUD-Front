a1437ecd594fa157c4cf2867fcf75a901ce7b975
Alan <devaliant94@gmail.com>
2020-06-25 20:42:24 -0300

    template svelte - Add product Form

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<main>
+
+  <form>
+    <input type="text" placeholder="Product Name" id="product-name" />
+    <textarea
+      id="product-description"
+      rows="3"
+      placeholder="Product Description" />
+    <input
+      type="url"
+      id="product-img-url"
+      placeholder="https://example.com/image.jpg" />
+    <button>
+		Save Product
+	</button>
+  </form>
+
+</main>
