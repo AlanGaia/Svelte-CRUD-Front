52b70c4ec59b5fb076ead76f55532147a8b2fb73
Alan <devaliant94@gmail.com>
2020-06-25 21:50:40 -0300

    added bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -8,10 +8,9 @@
   };
 
   const submitHandler = e => {
-	e.preventDefault();
-	console.log(product);
-  }
-
+    e.preventDefault();
+    console.log(product);
+  };
 </script>
 
 <style>
@@ -20,28 +19,58 @@
 
 <main>
 
-  <form on:submit={submitHandler}>
-    <input
-      bind:value={product.name}
-      type="text"
-      placeholder="Product Name"
-      id="product-name" />
-    <textarea
-      bind:value={product.description}
-      id="product-description"
-      rows="3"
-      placeholder="Product Description" />
-    <select id="product-category" bind:value={product.category}>
-      <option value="notebooks">Notebooks</option>
-      <option value="peripherals">Peripherals</option>
-      <option value="components">Components</option>
-    </select>
-    <input
-      bind:value={product.imageURL}
-      type="url"
-      id="product-img-url"
-      placeholder="https://example.com/image.jpg" />
-    <button>Save Product</button>
-  </form>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6" />
+
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <form on:submit={submitHandler}>
+
+              <div class="form-group">
+                <input
+                  bind:value={product.name}
+                  type="text"
+                  placeholder="Product Name"
+                  id="product-name"
+                  class="form-control" />
+              </div>
+              <div class="form-group">
+                <textarea
+                  bind:value={product.description}
+                  id="product-description"
+                  rows="3"
+                  placeholder="Product Description"
+                  class="form-control" />
+              </div>
+              <div class="form-group">
+                <select
+                  id="product-category"
+                  bind:value={product.category}
+                  class="form-control">
+                  <option value="notebooks">Notebooks</option>
+                  <option value="peripherals">Peripherals</option>
+                  <option value="components">Components</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <input
+                  bind:value={product.imageURL}
+                  type="url"
+                  id="product-img-url"
+                  placeholder="https://example.com/image.jpg"
+                  class="form-control" />
+              </div>
+
+              <button>Save Product</button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 </main>
