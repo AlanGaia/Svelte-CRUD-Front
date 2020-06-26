2f8b1a33d9c031af51c5219c8ab082d904be6768
Alan <devaliant94@gmail.com>
2020-06-25 21:18:05 -0300

    finished form - event handler submit

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,16 @@
 <script>
+  let product = {
+    id: "",
+    name: "",
+    description: "",
+    category: "",
+    imageURL: ""
+  };
+
+  const submitHandler = e => {
+	e.preventDefault();
+	console.log(product);
+  }
 
 </script>
 
@@ -8,19 +20,28 @@
 
 <main>
 
-  <form>
-    <input type="text" placeholder="Product Name" id="product-name" />
+  <form on:submit={submitHandler}>
+    <input
+      bind:value={product.name}
+      type="text"
+      placeholder="Product Name"
+      id="product-name" />
     <textarea
+      bind:value={product.description}
       id="product-description"
       rows="3"
       placeholder="Product Description" />
+    <select id="product-category" bind:value={product.category}>
+      <option value="notebooks">Notebooks</option>
+      <option value="peripherals">Peripherals</option>
+      <option value="components">Components</option>
+    </select>
     <input
+      bind:value={product.imageURL}
       type="url"
       id="product-img-url"
       placeholder="https://example.com/image.jpg" />
-    <button>
-		Save Product
-	</button>
+    <button>Save Product</button>
   </form>
 
 </main>
